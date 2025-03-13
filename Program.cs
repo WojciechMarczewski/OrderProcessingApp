35b54223cc17a339754d2a88e222cd0f5b077648
Voyti <108059127+WojciechMarczewski@users.noreply.github.com>
2025-03-13 23:38:03 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OrderProcessingApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
