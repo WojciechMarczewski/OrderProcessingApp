a814cc173e568590867948609aca9e1fd62a3191
Voyti <108059127+WojciechMarczewski@users.noreply.github.com>
2025-03-17 18:57:03 +0100

    Unit tests implementation for business rules.

---
+++ tests/OrderProcessingApp.Tests/BusinessRulesTests/OrderBusinessRulesTests.cs
@@ -0,0 +1,114 @@
+﻿using OrderProcessingApp.BusinessRules;
+using OrderProcessingApp.Models.Enums;
+using OrderProcessingApp.Tests.Factories;
+
+namespace OrderProcessingApp.Tests
+{
+    public class OrderBusinessRulesTests
+    {
+        [Fact]
+        public void CashOnDeliveryThresholdRule_ReturnsTrue_WhenTresholdExceeded_AndCashOnDelivery()
+        {
+            //Arrange
+            var order = new TestOrderFactory().CreateOrderWithAmountAndPaymentMethod((decimal)2500.01, PaymentMethod.CashOnDelivery);
+            var rule = new CashOnDeliveryThresholdRule();
+            //Act
+            var result = rule.IsViolated(order);
+            //Assert
+            Assert.True(result, "The rule should be violated for order amount exceeding the threshold with Cash On Delivery.");
+        }
+        [Fact]
+        public void CashOnDeliveryThresholdRule_ReturnsFalse_WhenThresholdExceeded_AndCard()
+        {
+            //Arrange
+            var order = new TestOrderFactory().CreateOrderWithAmountAndPaymentMethod((decimal)2500.01, PaymentMethod.Card);
+            var rule = new CashOnDeliveryThresholdRule();
+            //Act
+            var result = rule.IsViolated(order);
+            //Assert
+            Assert.False(result);
+        }
+        [Fact]
+        public void CashOnDeliveryThresholdRule_ReturnsFalse_WhenThresholdExceeded_AndCashOnDelivery()
+        {
+            //Arrange
+            var order = new TestOrderFactory().CreateOrderWithAmountAndPaymentMethod((decimal)2499.99, PaymentMethod.CashOnDelivery);
+            var rule = new CashOnDeliveryThresholdRule();
+            //Act
+            var result = rule.IsViolated(order);
+            //Assert
+            Assert.False(result);
+        }
+        [Fact]
+        public void OrderCanChangeStatusRule_ReturnsFalse_WhenOrderChangesFromNewToInStock()
+        {
+            //Arrange
+            var order = new TestOrderFactory().CreateOrderWithStatus(OrderStatus.New);
+            var rule = new OrderCanChangeStatusRule(OrderStatus.InStock);
+            //Act
+            var result = rule.IsViolated(order);
+            //Assert
+            Assert.False(result, "The rule shouldn't be violated when transitioning from New to InStock.");
+        }
+        [Fact]
+        public void OrderCanChangeStatusRule_ReturnFalse_WhenOrderChangesFromInStockToInShipment()
+        {
+            //Arrange
+            var order = new TestOrderFactory().CreateOrderWithStatus(OrderStatus.InStock);
+            var rule = new OrderCanChangeStatusRule(OrderStatus.InShipment);
+            //Act
+            var result = rule.IsViolated(order);
+            //Assert
+            Assert.False(result, "The rule shouldn't be violated when transitioning from InStock to InShipment.");
+        }
+        [Theory]
+        [InlineData(OrderStatus.InStock)]
+        [InlineData(OrderStatus.ReturnedToClient)]
+        [InlineData(OrderStatus.Error)]
+        [InlineData(OrderStatus.Unknown)]
+        [InlineData(OrderStatus.InShipment)]
+        [InlineData(OrderStatus.Closed)]
+        public void OrderCanChangeStatusRule_ReturnsTrue_WhenOrderStatusChange_From_IsInvalid_To_InStock(OrderStatus initialStatus)
+        {
+            //Arrange
+            var order = new TestOrderFactory().CreateOrderWithStatus(initialStatus);
+            var rule = new OrderCanChangeStatusRule(OrderStatus.InStock);
+            //Act
+            var result = rule.IsViolated(order);
+            //Assert
+            Assert.True(result);
+        }
+        [Theory]
+        [InlineData(OrderStatus.New)]
+        [InlineData(OrderStatus.ReturnedToClient)]
+        [InlineData(OrderStatus.Error)]
+        [InlineData(OrderStatus.Unknown)]
+        [InlineData(OrderStatus.InShipment)]
+        [InlineData(OrderStatus.Closed)]
+        public void OrderCanChangeStatusRule_ReturnsTrue_WhenOrderStatusChange_From_IsInvalid_To_InShipment(OrderStatus initialStatus)
+        {
+            //Arrange
+            var order = new TestOrderFactory().CreateOrderWithStatus(initialStatus);
+            var rule = new OrderCanChangeStatusRule(OrderStatus.InShipment);
+            //Act
+            var result = rule.IsViolated(order);
+            //Assert
+            Assert.True(result);
+        }
+        [Theory]
+        [InlineData("x", "x", "x", "")]
+        [InlineData("x", "x", " ", "x")]
+        [InlineData("x", "", "x", "x")]
+        [InlineData("", "x", "x", "x")]
+        public void ShippingAddressRequiredRule_IsViolated_WhenAdrressIsMissingField(string addressStreet, string addressCity, string addressZipCode, string addressCountry)
+        {
+            //Arrange
+            var order = new TestOrderFactory().CreateOrderWithAddress(addressStreet, addressCity, addressZipCode, addressCountry);
+            var rule = new ShippingAddressRequiredRule();
+            //Act
+            var result = rule.IsViolated(order);
+            //Assert
+            Assert.True(result);
+        }
+    }
+}
---
+++ tests/OrderProcessingApp.Tests/Factories/TestOrderFactory.cs
@@ -0,0 +1,78 @@
+﻿using OrderProcessingApp.DTOs;
+using OrderProcessingApp.Factories;
+using OrderProcessingApp.Models;
+using OrderProcessingApp.Models.Enums;
+
+namespace OrderProcessingApp.Tests.Factories
+{
+    public class TestOrderFactory : IOrderFactory
+    {
+        public Order CreateNewOrder(OrderData orderData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Order CreateOrder(OrderData orderData, List<OrderStatusChange> orderHistory)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Order> GenerateSeedData()
+        {
+            throw new NotImplementedException();
+        }
+        public Order CreateOrderWithAmountAndPaymentMethod(decimal amount, PaymentMethod paymentMethod)
+        {
+            OrderData orderData = new(
+                    productName: "x",
+                    amount: amount,
+                    currency_Code: "PLN",
+                    currency_Symbol: "zł",
+                    clientType: 0,
+                    addressStreet: "xTest 6",
+                    addressCity: "xTest 1",
+                    addressZipCode: "54-200",
+                    addressCountry: "xTest",
+                    paymentMethod: (int)paymentMethod
+                );
+
+            OrderFactory orderFactory = new OrderFactory();
+            return orderFactory.CreateNewOrder(orderData);
+        }
+        public Order CreateOrderWithStatus(OrderStatus orderStatus)
+        {
+            OrderData orderData = new(
+                 productName: "x",
+                    amount: 1,
+                    currency_Code: "PLN",
+                    currency_Symbol: "zł",
+                    clientType: 0,
+                    addressStreet: "xTest 6",
+                    addressCity: "xTest 1",
+                    addressZipCode: "54-200",
+                    addressCountry: "xTest",
+                    paymentMethod: 0);
+
+            var orderHistory = new List<OrderStatusChange>() { new OrderStatusChange(orderStatus, DateTimeOffset.Now) };
+            OrderFactory orderFactory = new OrderFactory();
+            return orderFactory.CreateOrder(orderData, orderHistory);
+        }
+        public Order CreateOrderWithAddress(string addressStreet, string addressCity, string addressZipCode, string addressCountry)
+        {
+            OrderData orderData = new(
+                 productName: "x",
+                    amount: 1,
+                    currency_Code: "PLN",
+                    currency_Symbol: "zł",
+                    clientType: 0,
+                    addressStreet: addressStreet,
+                    addressCity: addressCity,
+                    addressZipCode: addressZipCode,
+                    addressCountry: addressCountry,
+                    paymentMethod: 0);
+            return new OrderFactory().CreateNewOrder(orderData);
+
+        }
+
+    }
+}
